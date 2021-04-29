7be9accaf286dce16f3deb255b45e58e78776259
mvecerin <62032481+mvecerin@users.noreply.github.com>
2021-04-29 15:42:18 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 8081,
-    //URI: 'mongodb://localhost:27017/studentManager',
-    URI: 'mongodb+srv://jane:1003128857@cluster0.t8sft.mongodb.net/studentManager?retryWrites=true&w=majority',
-    SECRET: 'diamondsSpadesClubsHearts'
-};
